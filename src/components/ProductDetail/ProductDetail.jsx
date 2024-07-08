b315a2d8273fbb58625627226993151780b521da
Ignacio A. Gonzalez <iggy.dc17@gmail.com>
2024-07-07 20:20:48 -0600

    Improve asyncMock Products

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,9 +1,11 @@
+import './ProductDetail.css';
 
 const ProductDetail = ({ product }) => {
     
     
     return (
         <li className="product">
+            <img src={product.image} alt={product.name} className="img-product" />
             <p>{product.name}</p>
             <p>$ {product.price}</p>
         </li>
--- src/data/asyncMockProducts.js
+++ src/data/asyncMockProducts.js
@@ -1,47 +1,47 @@
 export const appleProducts = {
+
     Mac: [
-        { id: 1, name: 'MacBook Air M1', price: 999, year: 2020, description: 'Lightweight and powerful with M1 chip.', image: 'path_to_macbook_air_m1_image'},
-        { id: 2, name: 'MacBook Air M2', price: 1199, year: 2022, description: 'Next generation performance with M2 chip.', image: 'path_to_macbook_air_m2_image'},
-        { id: 3, name: 'MacBook Pro 13-inch', price: 1299, year: 2020, description: 'High performance for professionals with M1 chip.', image: 'path_to_macbook_pro_13_image'},
-        { id: 4, name: 'MacBook Pro 14-inch', price: 1999, year: 2021, description: 'Ultimate performance with M1 Pro chip.', image: 'path_to_macbook_pro_14_image'},
-        { id: 5, name: 'MacBook Pro 16-inch', price: 2499, year: 2021, description: 'Ultimate performance with M1 Max chip.', image: 'path_to_macbook_pro_16_image'},
-        { id: 6, name: 'iMac 24-inch', price: 1299, year: 2021, description: 'All-in-one desktop with M1 chip and stunning 4.5K Retina display.', image: 'path_to_imac_24_image'},
-        { id: 7, name: 'iMac 27-inch', price: 1799, year: 2020, description: 'Powerful all-in-one desktop with Intel processors and 5K Retina display.', image: 'path_to_imac_27_image'},
-        { id: 8, name: 'Mac Mini M1', price: 699, year: 2020, description: 'Compact desktop with M1 chip.', image: 'path_to_mac_mini_image'},
-        { id: 9, name: 'Mac Studio', price: 1999, year: 2022, description: 'High-performance desktop for creative professionals with M1 Max or M1 Ultra chip.', image: 'path_to_mac_studio_image'},
-        { id: 10, name: 'Mac Pro', price: 5999, year: 2019, description: 'Ultimate workstation with Intel Xeon processors and modular design.', image: 'path_to_mac_pro_image'}
+        { id: 1, name: 'MacBook Air M1', price: 999, year: 2020, description: 'Lightweight and powerful with M1 chip.', image: 'https://m.media-amazon.com/images/I/71fMZDz2sEL._AC_UF894,1000_QL80_.jpg'},
+        { id: 2, name: 'MacBook Air M2', price: 1199, year: 2022, description: 'Next generation performance with M2 chip.', image: 'https://www.apple.com/newsroom/images/product/mac/standard/Apple-WWDC22-MacBook-Air-lp-220606.jpg.og.jpg?202405240035'},
+        { id: 3, name: 'MacBook Pro 13-inch', price: 1299, year: 2020, description: 'High performance for professionals with M1 chip.', image: 'https://http2.mlstatic.com/D_NQ_NP_711351-MLM51375704853_092022-O.webp'},
+        { id: 4, name: 'MacBook Pro 14-inch', price: 1999, year: 2021, description: 'Ultimate performance with M1 Pro chip.', image: 'https://m.media-amazon.com/images/I/61wjh-I8dHL._AC_UF894,1000_QL80_.jpg'},
+        { id: 5, name: 'MacBook Pro 16-inch', price: 2499, year: 2021, description: 'Ultimate performance with M1 Max chip.', image: 'https://cdsassets.apple.com/live/SZLF0YNV/images/sp/111901_mbp16-gray.png'},
+        { id: 6, name: 'iMac 24-inch', price: 1299, year: 2021, description: 'All-in-one desktop with M1 chip and stunning 4.5K Retina display.', image: 'https://m.media-amazon.com/images/I/61eoyE0l9gS.jpg'},
+        { id: 7, name: 'iMac 27-inch', price: 1799, year: 2020, description: 'Powerful all-in-one desktop with Intel processors and 5K Retina display.', image: 'https://cdsassets.apple.com/live/SZLF0YNV/images/sp/111913_sp821-imac-27.png'},
+        { id: 8, name: 'Mac Mini M1', price: 699, year: 2020, description: 'Compact desktop with M1 chip.', image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-mini-og-image-202301?wid=1200&hei=630&fmt=jpeg&qlt=95&.v=1670361046478'},
+        { id: 9, name: 'Mac Studio', price: 1999, year: 2022, description: 'High-performance desktop for creative professionals with M1 Max or M1 Ultra chip.', image: 'https://www.apple.com/newsroom/images/product/mac/standard/Apple-Mac-Studio-Studio-Display-Photoshop-220308_big.jpg.slideshow-large_2x.jpg'},
+        { id: 10, name: 'Mac Pro', price: 5999, year: 2019, description: 'Ultimate workstation with Intel Xeon processors and modular design.', image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-mac-pro-2019_AV2?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1585849393420'}
     ],
 
     iPad: [
-        { id: 1, name: 'iPad Air', price: 599, year: 2020, description: 'Powerful and versatile.', image: 'path_to_ipad_air_image' },
-        { id: 2, name: 'iPad Pro 11-inch', price: 799, year: 2021, description: 'Ultimate iPad experience with 11-inch Liquid Retina display.', image: 'path_to_ipad_pro_11_image' },
-        { id: 3, name: 'iPad Pro 12.9-inch', price: 999, year: 2021, description: 'Ultimate iPad experience with 12.9-inch Liquid Retina XDR display.', image: 'path_to_ipad_pro_12_image' },
-        { id: 4, name: 'iPad Mini', price: 499, year: 2021, description: 'Compact, powerful, and portable.', image: 'path_to_ipad_mini_image' },
-        { id: 5, name: 'iPad 10.2-inch', price: 329, year: 2021, description: 'Affordable and powerful.', image: 'path_to_ipad_10_2_image' }
+        { id: 1, name: 'iPad Air', price: 599, year: 2020, description: 'Powerful and versatile.', image: 'https://www.apple.com/v/ipad/home/ck/images/overview/select/product-tile/pt_ipad_air__cr381zljqdiu_large.png' },
+        { id: 2, name: 'iPad Mini', price: 499, year: 2021, description: 'Compact, powerful, and portable.', image: 'https://www.apple.com/v/ipad/home/ck/images/overview/select/product-tile/pt_ipad_mini__f3iy3qb50gia_large.png' },
+        { id: 3, name: 'iPad', price: 329, year: 2021, description: 'Affordable and powerful.', image: 'https://www.apple.com/v/ipad/home/ck/images/overview/select/product-tile/pt_ipad_10th_gen__ej5p5x6yf2gm_large.png' },
+        { id: 4, name: 'iPad Pro', price: 329, year: 2024, description: 'Thinpossible', image: 'https://www.apple.com/v/ipad/home/ck/images/overview/select/product-tile/pt_ipad_pro__6bgrkek0jnm2_large.png' }
     ],
 
     iPhone: [
-        { id: 1, name: 'iPhone 14 Pro Max', price: 1099, year: 2023, description: 'Pro camera system and Super Retina XDR display.', image: 'path_to_iphone_14_pro_max_image' },
-        { id: 2, name: 'iPhone 14 Pro', price: 999, year: 2023, description: 'Pro camera system and Super Retina XDR display.', image: 'path_to_iphone_14_pro_image' },
-        { id: 3, name: 'iPhone 14', price: 799, year: 2023, description: 'The latest iPhone.', image: 'path_to_iphone_14_image' },
-        { id: 4, name: 'iPhone SE (2022)', price: 399, year: 2022, description: 'Compact and affordable.', image: 'path_to_iphone_se_2022_image' },
-        { id: 5, name: 'iPhone 13 Pro Max', price: 1099, year: 2021, description: 'Pro camera system and Super Retina XDR display.', image: 'path_to_iphone_13_pro_max_image' },
-        { id: 6, name: 'iPhone 13 Pro', price: 999, year: 2021, description: 'Pro camera system and Super Retina XDR display.', image: 'path_to_iphone_13_pro_image' },
-        { id: 7, name: 'iPhone 13', price: 699, year: 2021, description: 'Advanced dual-camera system.', image: 'path_to_iphone_13_image' },
-        { id: 8, name: 'iPhone 12 Mini', price: 699, year: 2020, description: 'Compact and powerful.', image: 'path_to_iphone_12_mini_image' },
-        { id: 9, name: 'iPhone 15', price: 999, year: 2024, description: 'A camera that captures your wildest imagination.', image: 'path_to_iphone_12_mini_image' },
-        { id: 10, name: 'iPhone 15 Pro', price: 1200, year: 2024, description: 'Forged in titanium.', image: 'path_to_iphone_12_mini_image' },
-        { id: 11, name: 'iPhone 15 Pro Max', price: 1400, year: 2024, description: 'Game changing chip.', image: 'path_to_iphone_12_mini_image' }
+        { id: 1, name: 'iPhone 14 Pro Max', price: 1099, year: 2023, description: 'Pro camera system and Super Retina XDR display.', image: 'https://www.apple.com/v/iphone/home/bv/images/overview/select/iphone_14__cjgvgyn9cquu_large.png' },
+        { id: 2, name: 'iPhone 14 Pro', price: 999, year: 2023, description: 'Pro camera system and Super Retina XDR display.', image: 'https://www.apple.com/v/iphone/home/bv/images/overview/select/iphone_14__cjgvgyn9cquu_large.png' },
+        { id: 3, name: 'iPhone 14', price: 799, year: 2023, description: 'The latest iPhone.', image: 'https://www.apple.com/v/iphone/home/bv/images/overview/select/iphone_14__cjgvgyn9cquu_large.png' },
+        { id: 4, name: 'iPhone SE', price: 399, year: 2022, description: 'Compact and affordable.', image: 'https://www.apple.com/v/iphone/home/bv/images/overview/select/iphone_se__gmvp19w01mum_large.png' },
+        { id: 5, name: 'iPhone 13 Pro Max', price: 1099, year: 2021, description: 'Pro camera system and Super Retina XDR display.', image: 'https://www.apple.com/v/iphone/home/bv/images/overview/select/iphone_13__gnwdyqfq7i2y_large.png' },
+        { id: 6, name: 'iPhone 13 Pro', price: 999, year: 2021, description: 'Pro camera system and Super Retina XDR display.', image: 'https://www.apple.com/v/iphone/home/bv/images/overview/select/iphone_13__gnwdyqfq7i2y_large.png' },
+        { id: 7, name: 'iPhone 13', price: 699, year: 2021, description: 'Advanced dual-camera system.', image: 'https://www.apple.com/v/iphone/home/bv/images/overview/select/iphone_13__gnwdyqfq7i2y_large.png' },
+        { id: 8, name: 'iPhone 15', price: 999, year: 2024, description: 'A camera that captures your wildest imagination.', image: 'https://www.apple.com/v/iphone/home/bv/images/overview/select/iphone_14__cjgvgyn9cquu_large.png' },
+        { id: 9, name: 'iPhone 15 Pro', price: 1200, year: 2024, description: 'Forged in titanium.', image: 'https://www.apple.com/v/iphone/home/bv/images/overview/select/iphone_15_pro__bpnjhcrxofqu_large.png' },
+        { id: 10, name: 'iPhone 15 Pro Max', price: 1400, year: 2024, description: 'Game changing chip.', image: 'https://www.apple.com/v/iphone/home/bv/images/overview/select/iphone_15_pro__bpnjhcrxofqu_large.png' }
     ],
 
     AirPods: [
-        { id: 1, name: 'AirPods Pro', price: 249, year: 2019, description: 'Noise cancellation and superior sound.', image: 'path_to_airpods_pro_image' },
-        { id: 2, name: 'AirPods Max', price: 549, year: 2020, description: 'High-fidelity audio.', image: 'path_to_airpods_max_image' },
-        { id: 3, name: 'AirPods (3rd generation)', price: 179, year: 2021, description: 'Spatial audio with dynamic head tracking.', image: 'path_to_airpods_3rd_gen_image' }
+        { id: 1, name: 'AirPods 2nd Generation', price: 129, year: 2019, description: 'Noise cancellation and superior sound.', image: 'https://m.media-amazon.com/images/I/710rzW2RGcL.__AC_SX300_SY300_QL70_ML2_.jpg' },
+        { id: 2, name: 'AirPods 3rh Generation', price: 169, year: 2020, description: 'High-fidelity audio.', image: 'https://m.media-amazon.com/images/I/61fTopgP4bL._AC_UF350,350_QL80_.jpg' },
+        { id: 3, name: 'AirPods Pro', price: 249, year: 2021, description: 'Spatial audio with dynamic head tracking.', image: 'https://m.media-amazon.com/images/I/51DMT40Xr2L.__AC_SY300_SX300_QL70_ML2_.jpg' },
+        { id: 4, name: 'AirPods Max', price: 549, year: 2023, description: 'Spatial audio with dynamic head tracking.', image: 'https://m.media-amazon.com/images/I/81thV7SoLZL._AC_UF894,1000_QL80_.jpg' }
     ],
-    
+
     Vision: [
-        { id: 1, name: 'Apple Vision Pro', price: 3499, year: 2024, description: 'Augmented reality headset.' }
+        { id: 1, name: 'Apple Vision Pro', price: 3499, year: 2024, description: 'Augmented reality headset.', image: 'https://m.media-amazon.com/images/I/61Rit1LERjL._AC_UF894,1000_QL80_.jpg' }
     ]
 };
 
