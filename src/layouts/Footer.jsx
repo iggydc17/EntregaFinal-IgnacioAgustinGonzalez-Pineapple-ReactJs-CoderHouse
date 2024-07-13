0dfc5e79ffa32094b5875c8e47aeea3e756921a1
Ignacio A. Gonzalez <iggy.dc17@gmail.com>
2024-07-13 15:13:39 -0600

    update Footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,14 +1,45 @@
+import { Link } from "react-router-dom";
+import './Footer.css'
 
 const Footer = () => {
 
     return (
 
         <footer>
-            <div className="info-box">
-                <p><a href="#">About Pineapple</a> | <a href="#">Contact Us</a></p>
+            <hr />
+            <h5 className="footer-title">Shop And Learn</h5>
+            <div className="footer-navbar">
+                <nav>
+                    <ul className="footer-links-list">
+                        <li>
+                            <Link to={"/"}>Home</Link>
+                        </li>
+                        <li>
+                            <Link to={"/store"}>Store</Link>
+                        </li>
+                        <li>
+                            <Link to={"#"}>Mac</Link>
+                        </li>
+                        <li>
+                            <Link to={"#"}>iPad</Link>
+                        </li>
+                        <li>
+                            <Link to={"#"}>iPhone</Link>
+                        </li>
+                        <li>
+                            <Link to={"#"}>Vision</Link>
+                        </li>
+                        <li>
+                            <Link to={"#"}>Accessories</Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
             <hr />
-            <p className="copy">&copy; {new Date().getFullYear()} Pineapple Inc. All Rights Reserved</p>
+            <div className="copy-box">
+                <p className="copy">&copy; {new Date().getFullYear()} Pineapple Inc. All Rights Reserved</p>
+                <p><Link to="#">About Pineapple</Link> | <Link to="#">Contact Us</Link></p>                
+            </div>
         </footer>
     );
 }
